047cfb6db8bf9c904de73fc25e528ae066da60ce
HouJun11220033 <1144724015@qq.com>
2016-05-15 22:19:06 +0800

    5.15-完成登陆界面

---
+++ LMS/Form1.Designer.cs
@@ -0,0 +1,171 @@
+﻿namespace LMS
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.admin = new System.Windows.Forms.RadioButton();
+            this.teacher = new System.Windows.Forms.RadioButton();
+            this.student = new System.Windows.Forms.RadioButton();
+            this.username = new System.Windows.Forms.Label();
+            this.password = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(149, 129);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(164, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(149, 170);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(164, 20);
+            this.textBox2.TabIndex = 1;
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.BackColor = System.Drawing.Color.Transparent;
+            this.btnCancel.Location = new System.Drawing.Point(238, 245);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(75, 23);
+            this.btnCancel.TabIndex = 2;
+            this.btnCancel.Text = "取消";
+            this.btnCancel.UseVisualStyleBackColor = false;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.BackColor = System.Drawing.Color.Transparent;
+            this.btnLogin.Location = new System.Drawing.Point(149, 245);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(75, 23);
+            this.btnLogin.TabIndex = 3;
+            this.btnLogin.Text = "登陆";
+            this.btnLogin.UseVisualStyleBackColor = false;
+            this.btnLogin.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // admin
+            // 
+            this.admin.AutoSize = true;
+            this.admin.BackColor = System.Drawing.Color.Transparent;
+            this.admin.Location = new System.Drawing.Point(70, 212);
+            this.admin.Name = "admin";
+            this.admin.Size = new System.Drawing.Size(61, 17);
+            this.admin.TabIndex = 4;
+            this.admin.TabStop = true;
+            this.admin.Text = "管理员";
+            this.admin.UseVisualStyleBackColor = false;
+            // 
+            // teacher
+            // 
+            this.teacher.AutoSize = true;
+            this.teacher.BackColor = System.Drawing.Color.Transparent;
+            this.teacher.Location = new System.Drawing.Point(161, 212);
+            this.teacher.Name = "teacher";
+            this.teacher.Size = new System.Drawing.Size(49, 17);
+            this.teacher.TabIndex = 5;
+            this.teacher.TabStop = true;
+            this.teacher.Text = "教师";
+            this.teacher.UseVisualStyleBackColor = false;
+            // 
+            // student
+            // 
+            this.student.AutoSize = true;
+            this.student.BackColor = System.Drawing.Color.Transparent;
+            this.student.Location = new System.Drawing.Point(252, 212);
+            this.student.Name = "student";
+            this.student.Size = new System.Drawing.Size(49, 17);
+            this.student.TabIndex = 6;
+            this.student.TabStop = true;
+            this.student.Text = "学生";
+            this.student.UseVisualStyleBackColor = false;
+            // 
+            // username
+            // 
+            this.username.AutoSize = true;
+            this.username.BackColor = System.Drawing.Color.Transparent;
+            this.username.Location = new System.Drawing.Point(55, 129);
+            this.username.Name = "username";
+            this.username.Size = new System.Drawing.Size(43, 13);
+            this.username.TabIndex = 7;
+            this.username.Text = "用户名";
+            // 
+            // password
+            // 
+            this.password.AutoSize = true;
+            this.password.BackColor = System.Drawing.Color.Transparent;
+            this.password.Location = new System.Drawing.Point(55, 177);
+            this.password.Name = "password";
+            this.password.Size = new System.Drawing.Size(31, 13);
+            this.password.TabIndex = 8;
+            this.password.Text = "密码";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(398, 360);
+            this.Controls.Add(this.password);
+            this.Controls.Add(this.username);
+            this.Controls.Add(this.student);
+            this.Controls.Add(this.teacher);
+            this.Controls.Add(this.admin);
+            this.Controls.Add(this.btnLogin);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.Button btnLogin;
+        private System.Windows.Forms.RadioButton admin;
+        private System.Windows.Forms.RadioButton teacher;
+        private System.Windows.Forms.RadioButton student;
+        private System.Windows.Forms.Label username;
+        private System.Windows.Forms.Label password;
+    }
+}
+
---
+++ LMS/Form1.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace LMS
+{
+    public partial class Form1 : Form
+    {
+    //    string coustr = "service=.;database=LMS;usr=sa;pwd=root";
+    //    SqlConnection conn = new SqlConnection(coustr);
+    //    string sqlstr = "select * from cjb";
+    //    SqlCommand cmd = new SqlCommand(sqlstr,conn);
+    //    SqlDataAdapter reader = cmd.ExecuteNonQuery();
+    //    DataSet ds = new DataSet();
+    //    while(reader.Read())
+    //    {
+    //    string id=reader["学号"].ToString();
+    //}
+        static public string sn, sub;
+       
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //timer1.Start();
+        
+        }
+        
+        
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        Form2 fr2 = new Form2();
+        Form3 fr3 = new Form3();
+        Form4 fr4 = new Form4();
+
+
+    
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //string coustr = "service=.;database=LMS;usr=sa;pwd=root";
+            string coustr = "Data Source=DESKTOP-3THID4C;Initial Catalog=LMS;User ID=sa;PWD=root";
+            SqlConnection conn = new SqlConnection(coustr);
+            conn.Open();
+            if (textBox1.Text == "" || textBox2.Text == "") {
+                MessageBox.Show("信息不全，请不要遗漏信息!");
+            }
+            if (admin.Checked) {
+                string cstr = "select * from dlb where 类别='管理员' and 用户名='" + textBox1.Text.Trim() + "'";
+                SqlCommand comm = new SqlCommand(cstr, conn);
+                SqlDataReader dr = comm.ExecuteReader();
+                if (dr.Read())
+                {
+                    sn = textBox1.Text.Trim();
+                    fr4.Show();
+                    this.Visible = false;
+
+
+                }
+                else
+                {
+                    MessageBox.Show("密码或用户名出错，请重新输入！");
+                    textBox1.Text = "";
+                    textBox2.Text = "";
+                }
+            }
+            if (teacher.Checked)
+            {
+                string cstr = "select * from dlb where 类别='教师'and 用户名='" + textBox1.Text.Trim() + "'and 密码='" + textBox2.Text.Trim() + "'";
+                SqlCommand comm = new SqlCommand(cstr, conn);
+                SqlDataReader dr = comm.ExecuteReader();
+                if (dr.Read())
+                {
+                    sn = textBox1.Text.Trim();
+                    sub = dr.GetValue(3).ToString();
+                    fr3.Show();
+                    this.Visible = false;
+                }
+                else {
+                    MessageBox.Show("密码或用户名出错，请重新输入！");
+                    textBox1.Text = "";
+                    textBox2.Text = "";
+                
+                
+                }
+            }
+            if (student.Checked)
+            {
+                string cstr = "select * from dlb where 类别='学生' and 用户名='" + textBox1.Text.Trim() + "'and 密码='" + textBox2.Text.Trim() + "'";
+                SqlCommand comm = new SqlCommand(cstr,conn);
+                SqlDataReader dr = comm.ExecuteReader();
+                if (dr.Read())
+                {
+
+                    sn = textBox1.Text.Trim();
+                    fr2.Show();
+                    this.Visible = false;
+                    MessageBox.Show("Successful!");
+                }
+                else {
+
+                    MessageBox.Show("密码或用户名出错，请重新输入！");
+                    textBox1.Text = "";
+                    textBox2.Text = "";
+                }
+
+
+
+            }
+            conn.Close();
+            conn.Dispose();
+
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
